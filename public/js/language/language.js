9dcf9d71266aa1cc0040f815268e6ff56218c12c
Ranziroo <ajunf2@gmail.com>
2026-03-24 17:08:44 +0700

    new

---
+++ public/js/language/language.js
@@ -0,0 +1,13 @@
+const translations = {
+  id: {
+    temukan: "Temukan Akun Favoritmu",
+    search: "Cari akun...",
+    tidak: "Akun Tidak Ditemukan",
+
+  },
+  en: {
+    temukan: "Find Your Favorite Account",
+    search: "Search account...",
+    tidak: "Account Not Found",
+  }
+};
